4816ec9ab128c55a47295a7d800077d400c61205
3hd <3hd@yndx.space>
2015-09-01 15:55:57 +0300

    Initial

---
+++ proxy.rb
@@ -0,0 +1,64 @@
+require 'forwardable'
+
+# Provide a surrogate or placeholder for another object to control
+# access to it
+class Hero
+  attr_accessor :keywords
+
+  def initialize
+    @keywords = []
+  end
+end
+
+class ComputerProxy
+  # Forwardable allows objects to run methods on behalf
+  # of it's members, in this case the Computer object
+  extend Forwardable
+
+  # We delegate the ComputerProxy's use of
+  # the Computer object's add method
+  def_delegators :real_object, :add
+
+  def initialize(hero)
+    @hero = hero
+  end
+
+  def execute
+    check_access
+    real_object.execute
+  end
+
+  def check_access
+    unless @hero.keywords.include?(:computer)
+      raise "You have no access"
+    end
+  end
+
+  def real_object
+    @real_object ||= Computer.new
+  end
+end
+
+class Computer
+  def initialize
+    @queue = []
+  end
+
+  def add(command)
+    @queue << command
+  end
+
+  def execute
+    "executing commands"
+  end
+end
+
+# Usage
+hero = Hero.new
+proxy = ComputerProxy.new(hero)
+proxy.add("some command")
+proxy.execute
+# => raise error
+hero.keywords << :computer
+proxy.execute
+# => executing commands
---
+++ state.rb
@@ -0,0 +1,52 @@
+#This pattern try to simplify complicate control flows changing an object's behaviour dynamically
+class Operation
+  attr_reader :state
+  def initialize
+    @state = OperationOpenState.new
+  end
+
+  def trigger(state)
+    @state = @state.next(state)
+  end
+end
+
+class OperationOpenState
+  def next(state)
+    if valid?(state)
+      OperationPendingPaymentState.new
+    else
+      raise IllegalStateJumpError
+    end
+  end
+
+  def valid?(state)
+    state == :pending_payment
+  end
+end
+
+class OperationPendingPaymentState
+  def next(state)
+    OperationConfirmState.new if valid?(state)
+  end
+
+  def valid?(state)
+    state == :confirm
+  end
+end
+class IllegalStateJumpError < StandardError; end
+class OperationConfirmState; end
+
+#Usage
+operation = Operation.new
+puts operation.state.class
+#=> OperationOpenState
+operation.trigger :pending_payment
+puts operation.state.class
+#=> OperationPendingPaymentState
+operation.trigger :confirm
+puts operation.state.class
+#=> OperationConfirmState
+
+operation = Operation.new
+operation.trigger :confirm
+#=> raise IllegalStateJumpError
